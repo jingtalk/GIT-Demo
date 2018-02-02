3a806fba1c7184cc5f552a1908e693f30a93721d
hakimhuang <hakimhuang@tencent.com>
2018-02-02 20:14:05 +0800

    add kotlin sample

---
+++ KotlinSample/src/main/kotlin/com/jing/kotlin/sample/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.jing.kotlin.sample
+
+/**
+ * <pre>
+ *     author : jinghuang
+ *     e-mail : jinghuang1130@yahoo.com
+ *     time   : 2018/02/02
+ *     desc   : 功能
+ *     version: 1.0
+ * </pre>
+ */
