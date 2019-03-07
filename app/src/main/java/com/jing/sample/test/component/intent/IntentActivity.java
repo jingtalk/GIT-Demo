333b5ef314a52594f9f06928634d48aa9cb5827f
hakimhuang <hakimhuang@tencent.com>
2019-03-07 14:44:33 +0800

    工程调整及git同步

---
+++ app/src/main/java/com/jing/sample/test/component/intent/IntentActivity.java
@@ -0,0 +1,24 @@
+package com.jing.sample.test.component.intent;
+
+import android.os.Bundle;
+
+import com.jing.sample.notimportant.BaseActivity;
+
+/**
+ * <pre>
+ *     author : jinghuang
+ *     e-mail : jinghuang1130@yahoo.com
+ *     time   : 2017/12/22
+ *     desc   : 功能
+ *     version: 1.0
+ * </pre>
+ */
+public class IntentActivity extends BaseActivity {
+
+    private static final String TAG = "IntentActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
